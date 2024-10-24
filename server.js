845e4afe4a6a001df15258f25e34afb4c84c2b3c
doca41531 <doca41531@gmail.com>
2024-10-24 17:31:17 +0900

    first

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 5500;
+
+// app.use(express.static(path.join(__dirname, "map")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "map", "main.html"));
+});
+
+app.listen(port, () => {
+  console.log(`서버가 실행됩니다. http://localhost:${port}`);
+});
